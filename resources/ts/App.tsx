dcf773915be482d01b2a41529bc7f8c3c9d279cd
nagao <dekufull@gmail.com>
2022-08-25 16:07:49 +0900

    reactrouteでページを分ける

---
+++ resources/ts/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Router from "./router";
+
+const App = () => {
+    return (
+        <Router />
+    )
+}
+
+export default App
